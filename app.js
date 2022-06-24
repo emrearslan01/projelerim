d4b47f9885c06a23ad79828f1715bd6bf3a9bd97
emre.arslan <emre.arslan@eld.local>
2022-06-24 15:37:08 +0300

    kodun son hali

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const spaceship=document.querySelector(".spaceship")
 const pscore=document.querySelector(".score p")
 
 //sound effects
-let laser=new Audio("laser.mp3")
-let boom=new Audio("boom.mp3")
+let laser=new Audio("./voices/laser.mp3")
+let boom=new Audio("./voices/boom.mp3")
 
 //game
 let move
@@ -13,11 +13,14 @@ let x,y
 let velocity=1.00
 
 //spaceship moving
-
+ //ridvan.ozpamukcu@forte.com.tr
 window.addEventListener("mousemove",(e)=>{
+   
    x=e.clientX;
    y=e.clientY;
    spaceship.style.left=x+"px"
+
+   
    spaceship.style.top=y+"px"
 
 

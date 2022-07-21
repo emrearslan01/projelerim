b751e2cc3b790d75b7bb98f71fd2b93d51cf8c62
emrearslan01 <ynsemrearsln23@gmail.com>
2022-07-21 16:15:13 +0300

    ilk commit

--- app.js
+++ app.js
@@ -1,121 +1,190 @@
-const main=document.querySelector(".main")
-const spaceship=document.querySelector(".spaceship")
-const pscore=document.querySelector(".score p")
+const main = document.querySelector(".main")
+const spaceship = document.querySelector(".spaceship")
+const pscore = document.querySelector(".score p")
+
+const modal = document.querySelector(".modal")
+const endingmodal = document.querySelector(".modal2")
+
+
+const startbutton = document.querySelector(".startgame")
+
 
 //sound effects
-let laser=new Audio("./voices/laser.mp3")
-let boom=new Audio("./voices/boom.mp3")
-
+let laser = new Audio("./voices/laser.mp3")
+let boom = new Audio("./voices/boom.mp3")
+let font = new Audio("./voices/font.mp3")
+let gameover = new Audio("./voices/gameover.mp3")
 //game
-let move
-let score=0
-let x,y
-let velocity=1.00
+let move;
+let score = 0;
+let x, y;
+let velocity = 1.00;
 
 
 //spaceship moving
-//mainj olarak değişcek
-window.addEventListener("mousemove",(e)=>{
-   x=e.clientX;
-   y=e.clientY;
-   spaceship.style.left=x+"px"
-   
-   if(y>window.innerHeight/2)
-      spaceship.style.top=y+"px"
-      
+
+window.addEventListener("mousemove", (e) => {
+   x = e.clientX;
+   y = e.clientY;
+   spaceship.style.left = x + "px"
+
+   if (y > window.innerHeight / 2)
+      spaceship.style.top = y + "px"
+
 });
-   
-   
-window.addEventListener("click",()=>{
-   velocity=velocity+0.5;
-   let fireball =document.createElement("div");
+
+
+window.addEventListener("click", () => {
+   velocity = velocity + 0.5;
+   let fireball = document.createElement("div");
    fireball.classList.add("fireball");
-   fireball.style.left=x+"px";
-   fireball.style.top=y-50+"px";
-   laser.play()
-   laser.currentTime=-2;
-  
-main.appendChild(fireball);
+   fireball.style.left = x + "px";
+   fireball.style.top = y - 60 + "px";
+   laser.play();
+   laser.currentTime = -2;
 
-setInterval(()=>{
-   
-let fireballtop=parseInt(window.getComputedStyle(fireball).getPropertyValue("top"))
-fireball.style.top=fireballtop -5+"px";
-if(fireballtop<0){
-      main.removeChild(fireball);
+   main.appendChild(fireball);
 
-}
+   setInterval(() => {
+
+      let rocks = document.querySelectorAll(".rock");
+      for (let i = 0; i < rocks.length; i++) {
+         let rock = rocks[i];
+         let rockbounds = rock.getBoundingClientRect();
+         let fireballbounds = fireball.getBoundingClientRect();
+
+         if (fireballbounds.left >= rockbounds.left
+            && fireballbounds.right <= rockbounds.right
+            && fireballbounds.top <= rockbounds.top
+            && fireballbounds.bottom <= rockbounds.bottom) {
+
+            main.removeChild(fireball);
+            setTimeout(() => {
+               main.removeChild(rock);
+               score = score + 10;
+               pscore.textContent = score;
+
+            }, 500);
+            boom.play();
+            boom.currentTime = 0;
+            rock.classList.add("asteroidexplode");
+
+         }
+      }
+
+
+      let fireballtop = parseInt(window.getComputedStyle(fireball).getPropertyValue("top"))
+      fireball.style.top = fireballtop - 7 + "px";
+      if (fireballtop < 0) {
+         main.removeChild(fireball);
+
+      }
+   });
 });
-});
 
-const enemymove=()=>{
-   move=setInterval(()=>{
-      let enemy=document.createElement("div")
-      enemy.classList.add("enemy")
-      let left=Math.floor(Math.random()*window.innerWidth-50)
-      enemy.style.left=left+"px";
-      enemy.style.top="px";
-      main.appendChild(enemy);
 
-let enemycount=document.querySelectorAll(".enemy")
-for(let i=1;i<=enemycount.length;i++)
-{
-      
-   let enemytop=parseInt(
-      window.getComputedStyle(enemycount[i-1]).getPropertyValue("top"))
-   let enemybottom=parseInt(
-      window.getComputedStyle(enemycount[i-1]).getPropertyValue("top"))
-      
-   //METEORLARIN geliş sıklığı
-   enemycount[i-1].style.top=enemytop+100+"px"
+const rockmove = () => {
+   move = setInterval(() => {
 
-   //esas önemli kısmımız
-   //değme kısımları temaslar tespit edilir
-let mainframe=main.getBoundingClientRect();
-if(enemybottom>mainframe.bottom){
-         setTimeout((e)=>{
-               main.removeChild(enemycount[i-1]);
-           },600);
-           /*
-           boom.play()
-           //çalar çalmaz dur
-           boom.currentTime=0;
-           */
-           enemycount[i-1].classList.add("asteroidexplode");
-        }
-        
-       }
-       checkshipcollition();
-      },450/velocity);
-      
+      let v = 10;
+      let rock = document.createElement("div");
+      rock.classList.add("rock");
+      let left = Math.floor(Math.random() * window.innerWidth - 50);
+      rock.style.left = left + "px";
+      rock.style.top = -50 + "px";
+      main.appendChild(rock);
+      let rockcount = document.querySelectorAll(".rock");
+      for (let i = 1; i <= rockcount.length; i++) {
+         font.play();
+         let rocktop = parseInt(window.getComputedStyle(rockcount[i - 1]).getPropertyValue("top"));
+         let rockbottom = parseInt(window.getComputedStyle(rockcount[i - 1]).getPropertyValue("top"))
+
+
+         rockcount[i - 1].style.top = rocktop + 20 + "px";
+
+
+         let mainframe = main.getBoundingClientRect();
+         if (rockbottom > mainframe.bottom - 50) {
+            setTimeout(() => {
+               modal.classList.remove("closemodal");
+               main.removeChild(rockcount[i - 1]);
+
+            }, 600);
+            boom.play();
+            boom.currentTime = 0;
+            rockcount[i - 1].classList.add("asteroidexplode");
+            clearInterval(move);
+
+
+         }
+
+      }
+      checkcraftcollition();
+      font.pause();
+      gameover.play();
+      endmodal.classList.add("endmodal");
+
+   }, 450 / velocity)
+
 };
 
-//çarpma kontrolü
-
-const checkshipcollition=()=>{
-   let enemies=document.querySelectorAll(".enemy");
+const checkcraftcollition = () => {
+   let rocks = document.querySelectorAll(".rock");
    {
-      for(let i=0;i<=enemy.length;i++){
-         let enemy=enemies[i]
-         let enemybounds=enemy.getBoundingClientRect()
-         let shipbounds=spaceship.getBoundingClientRect()
+      for (let i = 0; i <= rocks.length; i++) {
+         let rock = rocks[i];
+         let rockbounds = rock.getBoundingClientRect();
+         let craftbounds = spaceship.getBoundingClientRect();
 
-         if(shipbounds.left<=enemybounds.left && shipbounds.right>=enemybounds.right 
-            && shipbounds.top<=enemybounds.top && shipbounds.bottom>=enemybounds.bottom)
-            {
-               setTimeout(()=>{
-                  spaceship.classList.remove("spaceshipexplode");
-               })
+         if (craftbounds.left <= rockbounds.right
+            && craftbounds.right >= rockbounds.left
+            && craftbounds.top <= rockbounds.bottom
+            && craftbounds.bottom >= rockbounds.top) {
+            setTimeout(() => {
                
-               boom.play()
-               boom.currentTime=0
-               spaceship.classList.add("spaceshipexplode");
-               //çarpınca hareket durur
-               clearInterval(move);
-            }
+               modal.classList.remove("closemodal");
+               spaceship.classList.remove("spaceshipexplode");
+
+            }, 450)
+           
+            boom.play();
+            boom.currentTime = 0;
+            spaceship.classList.add("spaceshipexplode");
+            clearInterval(move);
+
+            font.pause();
+            gameover.play();
+
+
+         }
+         
       }
    }
 };
 
-enemymove();
+
+startbutton.addEventListener("click", () => {
+   document.querySelectorAll(".rock").forEach(item => {
+      main.removeChild(item);
+   })
+   spaceship.style.left = "50%";
+   spaceship.style.bottom = "-30px";
+   velocity = 1.00;
+   modal.classList.add("closemodal");
+   
+   score = 0;
+   pscore.textContent = score;
+   rockmove();
+});
+
+function exit() {
+   window.close();
+}
+
+
+
+
+
+
+
 
